9d079acb6698601e980c4417c7499bf2c198ae79
Debanjan Basu <debanjanbasu2006@gmail.com>
2022-02-23 20:29:57 +1100

    added in basic home

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <p>This is home</p>
+    </>
+  );
+}
