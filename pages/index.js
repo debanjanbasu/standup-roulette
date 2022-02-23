a490423e8cee8ca4881dc0c93fbd3519042faedb
Debanjan Basu <debanjanbasu2006@gmail.com>
2022-02-23 20:42:00 +1100

    udpated name

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <p>This is home</p>
+      <p>This is home of standup-roulette</p>
     </>
   );
 }
